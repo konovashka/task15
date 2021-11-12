d226d935fc56a571d75ceee295fd59d932bb4b7a
konovashka <anton_konovalov_1@mail.ru>
2021-11-12 23:37:24 +0300

    Добавьте файлы проекта.

---
+++ task15/task15.cpp
@@ -0,0 +1,24 @@
+﻿// Поиск элемента равного заданному 
+#include <iostream>
+
+int main()
+{
+	const int n=5;
+	int a[n];
+	int c,fl,numb;
+	fl = 0;
+	scanf_s("%d", &c);
+	for (int i = 0; i < n; i++)
+	{
+		scanf_s("%d", &a[i]);
+	}
+	for (int i = 0; ((i < n) && (fl==0)); i++)
+	{
+		if (a[i] == c)
+		{
+			fl = 0;
+			numb = i+1;
+		}
+	}
+	printf("%d", numb);
+}
